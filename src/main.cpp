0a111a153c51a8890e8444cc3b4c48761b69d35d
Benito GR <bgr.online@posteo.net>
2021-04-14 21:01:15 +0200

    Add SDL to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
 
-int main(int, char**) {
-  printf("Welcome to Tetris!\n");
+int main(int argsCount, char** args) {
+  const uint32_t flags = SDL_WINDOW_OPENGL;
+  SDL_Window *window = SDL_CreateWindow("Tetris", 0, 0, 800, 600, flags);
+  SDL_GL_CreateContext(window);
+
+  bool running = true;
+  while (running) {
+    SDL_Event Event;
+    while (SDL_PollEvent(&Event)) {
+      if (Event.type == SDL_KEYDOWN) {
+        running = Event.key.keysym.sym != SDLK_ESCAPE;
+      }
+      else if (Event.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+
+    SDL_GL_SwapWindow(window);
+  }
   return 0;
 }

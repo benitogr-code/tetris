0add9529e3162ec9dea5d01286d827f720397b8b
Benito GR <bgr.online@posteo.net>
2021-04-18 20:34:49 +0200

    Add glm library

--- src/pch.h
+++ src/pch.h
@@ -5,5 +5,12 @@
 #include <string>
 #include <vector>
 
+// GLM
+#include <glm/common.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
 // Others
 #include "system/logger.h"

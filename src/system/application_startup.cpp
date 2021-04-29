6e17d873cc1c518dc3930db5c9a28cafb79ac45c
Benito GR <bgr.online@posteo.net>
2021-04-29 22:58:38 +0200

    Add freetype library to project

--- src/system/application_startup.cpp
+++ src/system/application_startup.cpp
@@ -2,6 +2,8 @@
 #include "file_utils.h"
 
 #include <SDL.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 int startApplication(const StartupParams& params) {
   Logger::init();
@@ -12,7 +14,12 @@ int startApplication(const StartupParams& params) {
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     LOG_ERROR("Fail to initialize SDL");
+    return -1;
+  }
 
+  FT_Library ft;
+  if (FT_Init_FreeType(&ft)) {
+    LOG_ERROR("Fail to initialize FreeType");
     return -1;
   }
 
@@ -25,6 +32,8 @@ int startApplication(const StartupParams& params) {
 
   application->shutdown();
   delete application;
+
+  FT_Done_FreeType(ft);
 
   SDL_Quit();
 
